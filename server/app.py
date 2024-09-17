abf87f78c471dd0486769db116ddc00a8c984c6b
Dakota <dakota.klasky@gmail.com>
2024-09-17 13:34:43 -0400

    added views

--- server/app.py
+++ server/app.py
@@ -4,5 +4,34 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:param>')
+def print_string(param):
+    print(param)
+    return param
+
+@app.route('/count/<int:param>')
+def count(param):
+    r = ''
+    for i in range(param):
+        r += str(i) + '\n'
+    return r
+
+@app.route('/math/<num1>/<operation>/<num2>')
+def math(num1,operation,num2):
+    if operation == "+":
+        return str(int(num1) + int(num2))
+    elif operation == '-':
+        return str(int(num1) - int(num2))
+    elif operation == '*':
+        return str(int(num1)*int(num2))
+    elif operation == 'div':
+        return str(int(num1)/int(num2))
+    elif operation == '%':
+        return str(int(num1)%int(num2))
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
